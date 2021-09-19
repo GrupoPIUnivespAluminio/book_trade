74c3b4cab149d4f6570db6d6dde049ca60068556
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-09-19 00:10:39 -0300

    initial styles for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: %i[index new create edit update]
   before_action :find_book, only: %i[show edit update]
 
   def index
