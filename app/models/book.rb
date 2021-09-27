5ee1684acb25117c1d9e59e6a4b225c29c08de3e
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-09-26 21:16:32 -0300

    validations on book

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,6 @@ class Book < ApplicationRecord
   has_one_attached :cover
   has_many_attached :images
   has_many :conversations, dependent: :destroy
+
+  validates :title, :description, :user, presence: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Book, type: :model do
   context 'associations' do
     it { is_expected.to belong_to(:user) }
   end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:user) }
+  end
 end
